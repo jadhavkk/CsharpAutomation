3af0c4538287bf304489c3c5bd90f9c4c86cb963
komalj <jadhavkk39@gmail.com>
2022-12-28 15:13:54 +0530

    Create Project

--- ShoppingCartProject/Program.cs
+++ ShoppingCartProject/Program.cs
@@ -9,23 +9,32 @@ namespace Maveric.Runner
         static void Main(string[] args)
         {
 
-            ShoppingCart sc1 = new ShoppingCart();
-            ShoppingCart sc2 = new ShoppingCart();
+            ShoppingCart sc1 = new ShoppingCart(7);
+            ShoppingCart sc2 = new ShoppingCart(4);
+
+            ShoppingCart sc= ShoppingCart.CreateInstance(10);
 
             sc1.ID = 101;
             sc1.descr = "Laptop";
-            sc1.quantity =5;
+            sc1.Quantity =5;
             sc1.price = 45000.00;
 
             sc2.ID = 102;
-            sc2.descr ="Mobile";
-            sc2.quantity =3;
+            sc2.descr = "Mobile";
+            sc2.Quantity = 3;
             sc2.price =25000.00;
+
+            
 
             sc1.PrintShoppCartDetails();
             sc2.PrintShoppCartDetails();
+           // sc3.PrintShoppCartDetails();
+
             sc1.DiscountDetails();
             sc2.DiscountDetails();
+
+          
+
 
 
 
--- ShoppingCartProject/ShoppingCart.cs
+++ ShoppingCartProject/ShoppingCart.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,36 +12,82 @@ namespace ShoppingCartProject
         public void ShopCart1() { }
         public int ID;
         public string descr;
-        public int quantity;
+        private int _quantity;
         public double price;
 
+        public void Program() {
+           _quantity = 3;
+           Console.WriteLine("Object Created");
 
-        public void DiscountDetails()
+             }
+        public ShoppingCart(int _quantity)
         {
-            if (quantity == 2 )
-            {
+            this._quantity = _quantity;
+        }
+        
 
-                double d = (price - (price * 10 / 100)) * quantity;
-                Console.WriteLine("Final price : " + d);
-            }
-            else if(quantity>=3 && quantity < 5){
-                double d = (price - (price * 15 / 100)) * quantity;
-                Console.WriteLine("Final price : "  +d);
-            }
-            else if(quantity == 5)
+
+        public int Quantity
+        {
+            set
             {
-                double d = (price - (price * 25 / 100)) * quantity;
-                Console.WriteLine("Final price : " + d);
+                if (value > 0)
+                {
+                    this._quantity = _quantity;
+                    
+
+                }
+
+                else
+                {
+
+                    Console.WriteLine("Quantity cannot be negative");
+                }
             }
         }
+
+                 public static ShoppingCart CreateInstance(int quantity)
+                    {
+            ShoppingCart sc = new ShoppingCart(quantity);
+            return sc;
+
+                    }
+
+                public void DiscountDetails()
+        {
+            if (_quantity == 2)
+            {
+
+                double d = (price - (price * 10 / 100)) * _quantity;
+                Console.WriteLine("Final price : " + d);
+            }
+            else if (_quantity >= 3 && _quantity < 5)
+            {
+                double d = (price - (price * 15 / 100)) * _quantity;
+                Console.WriteLine("Final price : " + d);
+            }
+            else if (_quantity == 5)
+            {
+                double d = (price - (price * 25 / 100)) * _quantity;
+                Console.WriteLine("Final price : " + d);
+            }
+            else
+            {
+                Console.WriteLine("No discount");
+            } }
+
+
 
         public void PrintShoppCartDetails()
         {
-            Console.WriteLine("Id:"+ID);
+            Console.WriteLine("Id:" + ID);
             Console.WriteLine("descr:" + descr);
-            Console.WriteLine("quantity:" + quantity);
+            Console.WriteLine("quantity:" + _quantity);
             Console.WriteLine("price:" + price);
 
         }
-    }
-}
+
+    } }
+             
+     
+        
